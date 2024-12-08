a4911813c7e7446c47d097273a8cfe55fb8a7dd6
senivan <sen.pn@ucu.edu.ua>
2024-12-08 11:14:28 -0800

    semi-automatic calibration

--- libserialcom.c
+++ libserialcom.c
@@ -9,99 +9,101 @@ uint16_t center = 0;
 uint16_t delta = 0;
 uint16_t left_max = 0;
 uint16_t right_max = 0;
-uint16_t break_start = 0;
-uint16_t break_end = 0;
-uint16_t acceleration_start = 0;
-uint16_t acceleration_end = 0;
+uint16_t break_start = 4100;
+uint16_t break_end = 1250;
+uint16_t acceleration_start = 4100;
+uint16_t acceleration_end = 1250;
 
 
-void calibrate_wheel(struct sp_port **port) {
-    printf("Rotate wheel to the left until he end!!! Then press 2 shifters...\n");
-	WheelSystemState temp1 = read_bytes(port);
-	//WheelSystemState temp1 = read_bytes(port);
-	//temp1.rotation = temp[0] | (temp[1] << 8);
-	//temp1.r_shift = temp[2] & 0x01;
-	//temp1.l_shift = (temp[2] >> 1) & 0x01;
-    while (temp1.r_shift != 1 || temp1.l_shift != 1) {
-		//temp = read_calibration(port);
-		temp1 = read_bytes(port);
-
-    }
-	left_max = temp1.rotation + 10;
-    printf("Release the shifters...\n");
-    while (temp1.r_shift != 0 || temp1.l_shift != 0) {
-		//temp = read_calibration(port);
-		temp1 = read_bytes(port);
-    }
-	printf("Rotate wheel to the right until he end!!! Then press 2 shifters...\n");
-	while (temp1.r_shift != 1 || temp1.l_shift != 1) {
-		temp1 = read_bytes(port);
-	}
-	right_max = temp1.rotation-10;
-	printf("Release the shifters...\n");
-	while (temp1.r_shift != 0 || temp1.l_shift != 0) {
-		temp1 = read_bytes(port);
-	}
-	break_start = temp1.breaking + 50;
-	printf("Press break to full.. then press 2 shifters\n");
-	while (temp1.r_shift != 1 || temp1.l_shift != 1) {
-		temp1 = read_bytes(port);
-	}
-	break_end = temp1.breaking - 50;
-	printf("Release the shifters...\n");
-	while (temp1.r_shift != 0 || temp1.l_shift != 0) {
-		temp1 = read_bytes(port);
-	}
-	acceleration_start = temp1.acceleration + 50;
-	printf("Press acceleration to full.. then press 2 shifters\n");
-	while (temp1.r_shift != 1 || temp1.l_shift != 1) {
-		temp1 = read_bytes(port);
-	}
-	acceleration_end = temp1.acceleration - 50;
-	printf("Press break to full");
-	printf("Calibration done!\n");
-	printf("Left max: %d\n", left_max);
-	printf("Right max: %d\n", right_max);
-	printf("Center: %d\n", (left_max + right_max) / 2);
-	center = (left_max + right_max) / 2;
-	delta = (right_max > center) ? right_max - center : left_max - center;
-	printf("Calibration done!\n");
-}
-
-//void calibrate_wheel(struct sp_port** port)
-//{
-//	uint8_t buf[7];
-//	uint8_t temp[1];
-//	printf("Rotate wheel to the left until he end!!!\n");
-//	temp[0] = 0x42;
-//	bool left_flag = false;
-//	bool right_flag = false;
-//	while (true) {
-//		sp_blocking_write(*port, temp, 1, 1000);
-//		sp_blocking_read(*port, buf, 7, 1000);
-//		uint16_t rotation = buf[0] | (buf[1] << 8);
-//		uint16_t acceleration = buf[2] | (buf[3] << 8);
-//		uint16_t breaking = buf[4] | (buf[5] << 8);
-//		uint8_t left_stop = buf[6] & 0x01;
-//		uint8_t right_stop = (buf[6] >> 1) & 0x01;
-//		printf("State: %d %d %d %d %d\n", rotation, acceleration, breaking, left_stop, right_stop);
-//		if (left_stop == 1) {
-//			left_flag = true;
-//			left_max = rotation - 10;
-//			printf("Now turn to the right\n");
-//		}
-//		if (right_stop == 1) {
-//			right_flag = true;
-//			printf("Now release the wheel\n");
-//			right_max = rotation + 10;
-//		}
-//		//if/* (left_flag && right_flag) {
-//		//	printf("Calibration done!\n");
-//		//	break;
-//		//}
+//void calibrate_wheel(struct sp_port **port) {
+//    printf("Rotate wheel to the left until he end!!! Then press 2 shifters...\n");
+//	WheelSystemState temp1 = read_bytes(port);
+//	//WheelSystemState temp1 = read_bytes(port);
+//	//temp1.rotation = temp[0] | (temp[1] << 8);
+//	//temp1.r_shift = temp[2] & 0x01;
+//	//temp1.l_shift = (temp[2] >> 1) & 0x01;
+//    while (temp1.r_shift != 1 || temp1.l_shift != 1) {
+//		//temp = read_calibration(port);
+//		temp1 = read_bytes(port);
+//
+//    }
+//	left_max = temp1.rotation + 10;
+//    printf("Release the shifters...\n");
+//    while (temp1.r_shift != 0 || temp1.l_shift != 0) {
+//		//temp = read_calibration(port);
+//		temp1 = read_bytes(port);
+//    }
+//	printf("Rotate wheel to the right until he end!!! Then press 2 shifters...\n");
+//	while (temp1.r_shift != 1 || temp1.l_shift != 1) {
+//		temp1 = read_bytes(port);
 //	}
-//	
+//	right_max = temp1.rotation-10;
+//	printf("Release the shifters...\n");
+//	while (temp1.r_shift != 0 || temp1.l_shift != 0) {
+//		temp1 = read_bytes(port);
+//	}
+//	break_start = temp1.breaking + 50;
+//	printf("Press break to full.. then press 2 shifters\n");
+//	while (temp1.r_shift != 1 || temp1.l_shift != 1) {
+//		temp1 = read_bytes(port);
+//	}
+//	break_end = temp1.breaking - 50;
+//	printf("Release the shifters...\n");
+//	while (temp1.r_shift != 0 || temp1.l_shift != 0) {
+//		temp1 = read_bytes(port);
+//	}
+//	acceleration_start = temp1.acceleration + 50;
+//	printf("Press acceleration to full.. then press 2 shifters\n");
+//	while (temp1.r_shift != 1 || temp1.l_shift != 1) {
+//		temp1 = read_bytes(port);
+//	}
+//	acceleration_end = temp1.acceleration - 50;
+//	printf("Press break to full");
+//	printf("Calibration done!\n");
+//	printf("Left max: %d\n", left_max);
+//	printf("Right max: %d\n", right_max);
+//	printf("Center: %d\n", (left_max + right_max) / 2);
+//	center = (left_max + right_max) / 2;
+//	delta = (right_max > center) ? right_max - center : left_max - center;
+//	printf("Calibration done!\n");
 //}
+
+void calibrate_wheel(struct sp_port** port)
+{
+	uint8_t buf[7];
+	uint8_t temp[1];
+	printf("Rotate wheel to the left until he end!!!\n");
+	temp[0] = 0x42;
+	bool left_flag = false;
+	bool right_flag = false;
+	while (true) {
+		sp_blocking_write(*port, temp, 1, 1000);
+		sp_blocking_read(*port, buf, 7, 1000);
+		uint16_t rotation = buf[0] | (buf[1] << 8);
+		uint16_t acceleration = buf[2] | (buf[3] << 8);
+		uint16_t breaking = buf[4] | (buf[5] << 8);
+		uint8_t left_stop = buf[6] & 0x01;
+		uint8_t right_stop = (buf[6] >> 1) & 0x01;
+		printf("State: %d %d %d %d %d\n", rotation, acceleration, breaking, left_stop, right_stop);
+		if (left_stop == 1) {
+			left_flag = true;
+			left_max = rotation + 10;
+			printf("Now turn to the right\n");
+		}
+		if (right_stop == 1) {
+			right_flag = true;
+			printf("Now release the wheel\n");
+			right_max = rotation - 10;
+		}
+		if (left_flag && right_flag) {
+			center = (left_max + right_max) / 2;
+			delta = (right_max > center) ? right_max - center : left_max - center;
+			printf("Calibration done!\n");
+			return;
+		}
+	}
+	
+}
 WheelSystemState read_bytes(struct sp_port **port) {
     WheelSystemState state;
     uint8_t buf[8];
@@ -144,7 +146,6 @@ WheelSystemState read_bytes(struct sp_port **port) {
 
 	memcpy(&state.acceleration, &buf[4], 2);
 	memcpy(&state.breaking, &buf[6], 2);
-	//printf("State: %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n", state.rotation, state.left_arr, state.right_arr, state.down_arr, state.up_arr, state.a_butt, state.b_butt, state.x_butt, state.y_butt, state.dl_butt, state.dr_butt, state.r_shift, state.l_shift, state.acceleration, state.breaking);
 
 	//printf("Rotation: %d\n", state.rotation);
 
@@ -155,6 +156,7 @@ WheelSystemState read_bytes(struct sp_port **port) {
 	state.rotation = (((state.rotation - center) * 32767 / delta)+32767) / 2;
 	state.breaking = ((state.breaking - break_start) * 32767 / (break_end - break_start));
 	state.acceleration = ((state.acceleration - acceleration_start) * 32767 / (acceleration_end - acceleration_start));
+	printf("State: %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n", state.rotation, state.left_arr, state.right_arr, state.down_arr, state.up_arr, state.a_butt, state.b_butt, state.x_butt, state.y_butt, state.dl_butt, state.dr_butt, state.r_shift, state.l_shift, state.acceleration, state.breaking);
 	return state;
 }
 
@@ -193,7 +195,7 @@ void send_ffb_bytes(struct sp_port *port, ffb_packet ffb) {
 
 struct sp_port* init_serial() {
 	struct sp_port* port = (struct sp_port*)malloc(sizeof(struct sp_port*));
-    sp_get_port_by_name("\\\\.\\COM6", &port);
+    sp_get_port_by_name("\\\\.\\COM3", &port);
     sp_open(port, SP_MODE_READ_WRITE);
     sp_set_baudrate(port, 115200);
 	sp_set_bits(port, 8);
